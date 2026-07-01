62065a3e7ac926612966c869da43a7a1efda29b1
Thar Htoo Naing <tharhtoonaing1998@gmail.com>
2026-07-01 19:38:15 +0200

    Hourly Hydrogen Production from Renewable Power

--- Hydrogen_Calculator.py
+++ Hydrogen_Calculator.py
@@ -1,4 +1,4 @@
-# Hydrogen Production Calculator with Efficiency
+# Hourly Hydrogen Production from Renewable Power
 
 def calculate_hydrogen(electrolyzer_power_kw, operation_hours, efficiency):
     h2_hhv_kwh_per_kg = 39.4  # kWh/kg H2
@@ -10,21 +10,28 @@ def calculate_hydrogen(electrolyzer_power_kw, operation_hours, efficiency):
     return electricity_used_kwh, useful_energy_kwh, hydrogen_produced_kg
 
 
-# Input values
-power_kw = 1000
-hours = 5
+# Input data
+renewable_power_kw = [0, 0, 100, 300, 600, 800, 700, 400, 100, 0]
 efficiency = 0.75
+operation_hours = 1  # each step is 1 hour
 
-# Run calculation
-electricity, useful_energy, hydrogen = calculate_hydrogen(
-    power_kw, hours, efficiency)
+total_hydrogen_kg = 0
+total_electricity_kwh = 0
 
-# Output
-print("Hydrogen Production Calculator")
-print("------------------------------")
-print("Electrolyzer power:", power_kw, "kW")
-print("Operation time:", hours, "hours")
-print("Efficiency:", efficiency * 100, "%")
-print("Electricity used:", electricity, "kWh")
-print("Useful hydrogen energy:", useful_energy, "kWh")
-print("Hydrogen produced:", round(hydrogen, 2), "kg")
+print("Hourly Hydrogen Production")
+print("--------------------------")
+
+for hour, power in enumerate(renewable_power_kw):
+    electricity, useful_energy, hydrogen = calculate_hydrogen(
+        power, operation_hours, efficiency)
+
+    total_hydrogen_kg += hydrogen
+    total_electricity_kwh += electricity
+
+    print("Hour:", hour,
+          "| Power:", power, "kW",
+          "| H2 produced:", round(hydrogen, 2), "kg")
+
+print("--------------------------")
+print("Total electricity used:", total_electricity_kwh, "kWh")
+print("Total hydrogen produced:", round(total_hydrogen_kg, 2), "kg")
